02b4e8849f743b1af2c24f23a56e3a4b37894a2a
Indrajit Pal <palindrajit10@gmail.com>
2024-02-29 00:57:43 +0530

    ok

--- Backend/index.js
+++ Backend/index.js
@@ -17,6 +17,13 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://modern-notes-app.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 database_url = 'mongodb+srv://palindrajit10:BR7ebSUlu9i9UPUZ@cluster0.3kxj1mm.mongodb.net/'
 mongoose.connect(database_url, {
 });
